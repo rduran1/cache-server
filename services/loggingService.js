2c8d06dd3a0d93df404e0cd3960fc1fde6219be6
Ruben Duran <rubenduran@onportal.net>
2021-03-15 09:15:40 -0500

    added padding to level value

--- services/loggingService.js
+++ services/loggingService.js
@@ -6,13 +6,14 @@ const configurationService = require('./configurationService');
 function logit(fileName, level, msg) {
 	const loggingLevel = configurationService.getLoggingLevels().map((e) => e.toUpperCase());
 	if (!loggingLevel.includes(level)) return;
-	appendFileSync(fileName, `${Date()}: [${level}] ${msg}${EOL}`);
+	const padding = ' '.repeat(5 - level.length);
+	appendFileSync(fileName, `${Date()}: [${level}${padding}] ${msg}${EOL}`);
 }
 
 function loggingService(fileName) {
 	if (typeof fileName !== 'string') throw new Error('Parameter must be of type string');
-	if (!fileName.includes('.js')) throw new Error('File name must end in ".js"');
-	const baseName = path.basename(fileName).replace('.js', '.log');
+	if (!fileName.endsWith('.js')) throw new Error('File name must end with ".js"');
+	const baseName = path.basename(fileName).replace(/\.js$/i, '.log');
 	const fileLocation = path.join(process.env.INSTALL_DIR, 'logs', baseName);
 	return {
 		info: (message) => { logit(fileLocation, 'INFO', message); },
