2d297c1994aa7d8e2dcb686e8d0a24947ef2f6e0
Ruben Duran <rubenduran@onportal.net>
2021-03-30 21:04:04 -0500

    removed configurationService due to circular reference

--- services/loggingService.js
+++ services/loggingService.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const { EOL } = require('os');
-const { appendFileSync } = require('fs');
-const configurationService = require('./configurationService');
+const { appendFileSync, readFileSync } = require('fs');
+
+let loggingLevel = [];
 
 function logit(fileName, level, msg) {
-	const loggingLevel = configurationService.getLoggingLevels().map((e) => e.toUpperCase());
-	if (!loggingLevel.includes(level)) return;
+	if (!loggingLevel.includes(level.toLowerCase())) return;
 	const padding = ' '.repeat(5 - level.length);
 	appendFileSync(fileName, `${Date()}: [${level}${padding}] ${msg}${EOL}`);
 }
 
 function loggingService(fileName) {
 	if (typeof process.env.INSTALL_DIR === 'undefined') throw new Error('Environmental variable INSTALL_DIR is undefined');
+	const config = readFileSync(path.join(process.env.INSTALL_DIR, 'models', 'stores', 'configurationStore.json'), 'utf-8');
+	loggingLevel = JSON.parse(config).loggingLevels;
 	if (typeof fileName !== 'string') throw new Error('Parameter must be of type string');
 	if (!fileName.endsWith('.js')) throw new Error('File name must end with ".js"');
 	const baseName = path.basename(fileName).replace(/\.js$/i, '.log');
