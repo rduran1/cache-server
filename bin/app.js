4627b39c764ebfd496cacc9c2368ae1b38642f0c
Ruben Duran <rubenduran@onportal.net>
2021-04-07 08:15:48 -0500

    move magic to config

--- bin/app.js
+++ bin/app.js
@@ -8,15 +8,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('../services/loggingService')(__filename);
 const config = require('../services/configurationService')().getExpressConfiguration();
 
-app.set('view engine', 'ejs');
+logger.debug(`process.env.INSTALL_DIR value: "${process.env.INSTALL_DIR}"`);
+
+app.set('view engine', config.viewEngine);
 app.set('views', path.join(process.env.INSTALL_DIR, config.viewsDirectory));
 
 app.use(cookieParser());
 app.use(express.static(config.staticDirectory));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: config.bodyParserUrlencodedExtended }));
 app.use(bodyParser.json({ limit: config.bodyParserJsonSizeLimit }));
 app.use(bodyParser.text({ limit: config.bodyParserTextSizeLimit }));
 app.use(helmet());
+
+app.disable('x-powered-by');
 
 function logIncomingRequest(req, res, next) {
 	const { connection: { remoteAddress }, originalUrl, method } = req;
@@ -30,19 +34,23 @@ app.use('/', require('../routes/hpsm'));
 // Error handler for middleware
 app.use((e, req, res, next) => {
 	if (!e) return next();
-	const { connection: { remoteAddress }, originalUrl } = req;
+	const { connection: { remoteAddress }, originalUrl, headers: { 'content-type': ct, 'content-length': cl } } = req;
+	let url = originalUrl;
+	if (config.filterTokenFromUrl) url = originalUrl.replace(/token=[0-9a-z]+/, 'token=[FILTERED]');
 	if (/^Failed to decode param/.test(e.message)) {
-		logger.error(`Request from ${remoteAddress} contains malformed URL: ${originalUrl.replace(/token=[0-9a-z]+/, 'token=[FILTERED]')}`);
-		return res.status(400).send(`${e.message}. Check URL encoding`);
+		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
+		return res.status(400).send(e.message);
+	}
+	if (/^Unexpected token /.test(e.message)) {
+		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
+		return res.status(400).send(e.message);
 	}
 	if (/request entity too large/.test(e.message)) {
-		logger.error(`Request from ${remoteAddress} generated HTTP 413 error: ${e.message}`);
-		return res.status(413).send('e.message');
+		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 413: ${e.message}`);
+		return res.status(413).send(e.message);
 	}
-	logger.error(`Request from ${remoteAddress} generated uncaught exception: ${e.message}`);
-	return res.status(500).send('Uncaught exception, check app.log and server.log for details');
+	logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Unhandled exception: ${e.message}`);
+	return res.status(500).send('Administrator notified of error, try again later');
 });
-
-app.disable('x-powered-by');
 
 module.exports = app;
