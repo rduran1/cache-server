5ffaaed988c294643ee0be2ff6e49d1d0fc84bdd
Ruben Duran <rubenduran@onportal.net>
2021-04-07 17:52:58 -0500

    removed sessionKey from config and hard coded

--- bin/app.js
+++ bin/app.js
@@ -20,13 +20,13 @@ app.set('view engine', config.viewEngine);
 app.set('views', path.join(process.env.INSTALL_DIR, config.viewsDirectory));
 
 app.use(cookieParser());
-app.use(express.static(config.staticDirectory));
+app.use(express.static(path.join(process.env.INSTALL_DIR, config.staticDirectory)));
 app.use(bodyParser.urlencoded({ extended: config.bodyParserUrlencodedExtended }));
 app.use(bodyParser.json({ limit: config.bodyParserJsonSizeLimit }));
 app.use(bodyParser.text({ limit: config.bodyParserTextSizeLimit }));
 app.use(helmet());
 app.use(session({
-	key: config.sessionKey,
+	key: 'user_sid',
 	secret: config.sessionSecret,
 	resave: config.sessionResave,
 	rolling: config.sessionRolling,
@@ -45,29 +45,37 @@ function logIncomingRequest(req, res, next) {
 	logger.info(`${remoteAddress} ${method} ${originalUrl.split('?')[0]}`);
 	next();
 }
-
 app.use(logIncomingRequest);
-app.use('/', require('../routes/hpsm'));
+
+const pc = require('../routes/appPreChecks');
+const appMain = require('../routes/appMain');
+const appLogin = require('../routes/appLogin');
+
+app.get('/', (req, res) => res.redirect('/app'));
+app.use('/app', pc.supportedBrowserCheck, pc.directBrowserToClearStaleSessionCookie, pc.checkForActiveSession, appMain);
+app.use('/app/login', appLogin);
+// app.use('/', require('../routes/hpsm'));
 
 // Error handler for middleware
 app.use((e, req, res, next) => {
 	if (!e) return next();
-	const { connection: { remoteAddress }, originalUrl, headers: { 'content-type': ct, 'content-length': cl } } = req;
+	// eslint-disable-next-line object-curly-newline
+	const { method, connection: { remoteAddress }, originalUrl, headers: { 'content-type': ct, 'content-length': cl } } = req;
 	let url = originalUrl;
 	if (config.filterTokenFromUrl) url = originalUrl.replace(/token=[0-9a-z]+/, 'token=[FILTERED]');
 	if (/^Failed to decode param/.test(e.message)) {
-		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
+		logger.error(`${remoteAddress} ${method} ${url} type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
 		return res.status(400).send(e.message);
 	}
 	if (/^Unexpected token /.test(e.message)) {
-		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
+		logger.error(`${remoteAddress} ${method} ${url} type "${ct}" length ${cl}B: Responding with HTTP 400: ${e.message}`);
 		return res.status(400).send(e.message);
 	}
 	if (/request entity too large/.test(e.message)) {
-		logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Responding with HTTP 413: ${e.message}`);
+		logger.error(`${remoteAddress} ${method} ${url} type "${ct}" length ${cl}B: Responding with HTTP 413: ${e.message}`);
 		return res.status(413).send(e.message);
 	}
-	logger.error(`${remoteAddress} url "${url}" type "${ct}" length ${cl}B: Unhandled exception: ${e.message}`);
+	logger.error(`${remoteAddress} ${method} ${url} type "${ct}" length ${cl}B: Unhandled exception: ${e.message}`);
 	return res.status(500).send('Administrator notified of error, try again later');
 });
 
